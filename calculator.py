d67c70d0785106406fa1645dc59224db0e52b0e8
Trey Wallace <contacttreyw@gmail.com>
2024-09-14 20:21:40 -0500

    enhanced calculator addition

--- calculator.py
+++ calculator.py
@@ -39,11 +39,11 @@ def calculator():
 
         if choice == '1':
             print(add(num1,num2))
-        if choice == '2':
+        elif choice == '2':
             print(subtract(num1,num2))
-        if choice == '3':
+        elif choice == '3':
             print(multiply(num1,num2))
-        if choice == '4':
+        elif choice == '4':
             print(divide(num1,num2))
 
 if __name__ == "__main__":
